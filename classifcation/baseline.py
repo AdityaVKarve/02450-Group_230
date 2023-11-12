52af8390766370365a04e2951dff61da10cd6f0f
erika <erika.maksimova@gmail.com>
2023-11-12 16:21:31 +0100

    feat: the multinomial regression model

--- classifcation/baseline.py
+++ classifcation/baseline.py
@@ -1,25 +1,13 @@
-import pandas as pd
+import nicotine_df
 from sklearn.model_selection import train_test_split
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-dataset = pd.read_csv('./Dataset/normalised_data.csv')
-
-predictors = ['Neuroticism (N-Score)', 'Extroversion (E-Score)', 'Openness (O-Score)', 'Agreeableness (A-Score)',
-              'Conscientiousness (C-Score)', 'Impulsiveness (BIS-11)', 'Sensation (SS)',
-              'Age_18-24', 'Age_25-34', 'Age_35-44', 'Age_45-54', 'Age_55-64', 'Age_65+',
-              'Gender_F', 'Gender_M', 'Country_Australia', 'Country_Canada', 'Country_Ireland', 'Country_New Zealand',
-              'Country_Other', 'Country_UK', 'Country_USA', 'Ethnicity_Asian', 'Ethnicity_Black', 'Ethnicity_Other',
-              'Ethnicity_White', 'Ethnicity_White-Asian', 'Ethnicity_White-Black',
-              'Education_A', 'Education_B', 'Education_C', 'Education_D', 'Education_E', 'Education_F',
-              'Education_G', 'Education_H', 'Education_I']
-predicates = ['Nicotine']
-total_set = predictors + predicates
-df = dataset[total_set]
+df = nicotine_df.df
 
 # Splitting data into training and test splits
-predictor_total_set = df[predictors]
-predicates_total_set = df[predicates]
+predictor_total_set = df[nicotine_df.predictors]
+predicates_total_set = df[nicotine_df.predicates]
 X_train, X_test, y_train, y_test = \
     train_test_split(predictor_total_set, predicates_total_set, test_size=0.33, random_state=42)
 
---
+++ classifcation/multinomial_regression.py
@@ -0,0 +1,40 @@
+import nicotine_df
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+df = nicotine_df.df
+
+# split data into training and test sets
+X = df[nicotine_df.predictors]  # personal data
+y = df[nicotine_df.predicates]  # nicotine
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# Define a range of lambda values to explore
+C_values = np.logspace(-3, 3, 7)  # This creates a range from 0.001 to 1000
+
+best_C = None
+best_accuracy = 0
+best_report = None
+
+for C in C_values:
+    # create and train the multinomial logistic regression model
+    multinomial_model = LogisticRegression(multi_class='multinomial', max_iter=5000, random_state=42, C=C)
+    multinomial_model.fit(X_train, y_train)
+
+    y_pred = multinomial_model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Check if this model has better accuracy than previous ones
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        best_report = classification_report(y_test, y_pred)
+        best_C = C
+
+# Print the best C value and corresponding accuracy
+print("Best C:", best_C)
+print("Best Accuracy:", best_accuracy)
+print("Classification Report:\n", best_report)
